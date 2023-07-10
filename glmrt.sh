f64f33704d6b76ea86496852b72090c9d28706e0
Shailendra Pandey <intelsp@live.com>
2023-07-10 15:22:51 +0530

    Update glmrt.sh

--- glmrt.sh
+++ glmrt.sh
@@ -1,6 +1,9 @@
 mkdir mlrt_setup
+mkdir mlrt
 cd mlrt_setup
 wget https://ssd.mathworks.com/supportfiles/downloads/R2022a/Release/6/deployment_files/installer/complete/glnxa64/MATLAB_Runtime_R2022a_Update_6_glnxa64.zip
 unzip glnxa64/MATLAB_Runtime_R2022a_Update_6_glnxa64.zip
-./install -agreeToLicense yes -destinationFolder /home/jovyan/mlrt -mode silent 
+./install -agreeToLicense yes -destinationFolder /home/jovyan/mlrt -mode silent
+cd ..
+rm mlrt_setup
  
